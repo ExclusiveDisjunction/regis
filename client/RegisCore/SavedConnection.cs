768d02cbba4ec7d86f92660133f0bef352e233dc
exdisj <exdisj@outlook.com>
2025-07-31 14:51:39 -0400

    Worked on message manager and connection details

--- client/RegisCore/SavedConnection.cs
+++ client/RegisCore/SavedConnection.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Configuration;
 
 namespace Regis {
     public class SavedConnection {
+        public required IPAddress Address { get; set; }
+        public required string Name { get; set; }
+    }
 
+    public class ConnectionManager {
+        private string configPath;
     }
 }
